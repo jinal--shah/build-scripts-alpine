ff31c1793fd710a5d22c39e17a92efd97cc0adf6
jinal--shah <jnshah@gmail.com>
2016-05-20 12:29:28 +0000

    moved basic vim setup to this repo, so can be shared with other images

---
+++ install_vim.sh
@@ -0,0 +1,35 @@
+#!env sh
+# install_vim.sh
+# suitable for apk pkg
+# - adds pathogen
+# - makes comments readable
+# - adds my preferred indentation defaults
+VIM_INDENT=${VIM_INDENT:-4}
+VIMRC=/etc/vim/vimrc
+
+apk add --no-cache vim &&
+rm -rf /var/cache/apk/* && 
+mkdir -p ~/.vim/autoload ~/.vim/bundle && 
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+if [[ ! -r ~/.vim/autoload/pathogen.vim ]]; then
+    echo "ERROR: $0: failed to get pathogen (vim plugin manager)"
+fi
+
+# ... we want modelines dammit ...
+sed -i 's/^\(set nomodeline\)/" \1/' $VIMRC
+
+cat << EOF >> $VIMRC
+" added by $0 $(date +'%Y-%m-%d %H:%M:%S')
+execute pathogen#infect()
+hi Comment ctermfg=6
+set tabstop=$VIM_INDENT
+set shiftwidth=$VIM_INDENT
+set shiftround
+set expandtab
+set smartindent
+set pastetoggle=<F1>
+autocmd FileType make setlocal noexpandtab
+EOF
+
+rm -rf /var/cache/apk/*
