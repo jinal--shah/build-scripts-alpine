3160e19bc7d5b50ee3bb5787aeec540b2ac823e1
jinal--shah <jnshah@gmail.com>
2016-06-16 09:37:03 +0000

    fixed up install_awscli to use wget without ssl_helper dependency

--- install_awscli.sh
+++ install_awscli.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # vim: et smartindent sr sw=4 ts=4:                                          #
 echo "$0: installing python, pip and awscli"                                 \
-&& apk --no-cache add python curl groff                                      \
+&& apk --no-cache add wget curl python groff less                            \
 && wget -q -T 10 -O /var/tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py \
 && python /var/tmp/get-pip.py                                                \
 && pip --no-cache-dir install --upgrade awscli                               \
+&& apk --purge del wget                                                      \
 && rm /var/tmp/get-pip.py                                                    \
 && aws --version                                                             #
