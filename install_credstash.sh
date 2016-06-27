db4614b9ad25cd76a02fe6d0e4e57b25315b0257
jinal--shah <jnshah@gmail.com>
2016-06-27 19:44:18 +0000

    fixed credstash installer

--- install_credstash.sh
+++ install_credstash.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # vim: et smartindent sr sw=4 ts=4:
 echo "$0: installing python, pip and credstash"                              \
-&& apk --no-cache add wget curl python groff less jq                         \
+&& apk --no-cache add wget curl python python-dev less                       \
 && wget -q -T 10 -O /var/tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py \
 && python /var/tmp/get-pip.py                                                \
 && pip --no-cache-dir install --upgrade credstash                            \
